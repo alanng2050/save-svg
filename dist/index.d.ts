1475b854d7c6ca0f138a8cc45042afd72c558cb6
alan <alanng2050>
2024-09-04 10:02:10 -0500

    update gitignore

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const saveSvg: ({ node, format, name, size, onFinish, }: {
-    node: Element | null;
-    format: 'png' | 'jpeg' | 'svg';
-    name: string;
-    size: number;
-    onFinish?: (() => void) | undefined;
-}) => Promise<void>;
